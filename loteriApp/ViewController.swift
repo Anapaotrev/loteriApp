ed73aa7bc23b6bd70afeb83c2c326aa18c493202
Ana Paola Treviño <anapao.trevino@gmail.com>
2019-03-20 17:41:08 -0600

    init project

---
+++ loteriApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loteriApp
+//
+//  Created by Ana Paola Treviño on 3/20/19.
+//  Copyright © 2019 Ana Paola Treviño. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
