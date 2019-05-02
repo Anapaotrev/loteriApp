0f795192df082401ff7d2f3acd2152f1cf36d92f
Ana Paola Treviño <anapao.trevino@gmail.com>
2019-05-01 19:29:32 -0500

    adds feature of des selecting cards

--- loteriApp/PlayViewController.swift
+++ loteriApp/PlayViewController.swift
@@ -18,6 +18,9 @@ class PlayViewController: UIViewController, UIScrollViewDelegate {
     var cardNames: NSArray!
     var formaGanar: String!
     
+    var cardsGenerated : Set = [-1]
+    var cardsSelected = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         generateCards()
@@ -36,7 +39,6 @@ class PlayViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func generateCards() {
-        var cardsGenerated : Set = [-1]
         //for every button
         for card in cardCollection {
             //generate a unique number
@@ -45,12 +47,14 @@ class PlayViewController: UIViewController, UIScrollViewDelegate {
             while(generatedCopy){
                 if(!cardsGenerated.contains(number)) {
                     cardsGenerated.insert(number)
+                    cardsSelected.append(number)
                     generatedCopy = false
                     //set button image according ot number
                     let dict = cardNames[number] as! NSDictionary
                     let image = UIImage(named: dict.value(forKey: "image") as! String)
                     card.setBackgroundImage(image, for: .normal)
                     card.setTitle(String(number), for: .normal)
+                    print(card.tag)
                 } else{
                     number = Int(arc4random_uniform(54))
                 }
@@ -58,23 +62,40 @@ class PlayViewController: UIViewController, UIScrollViewDelegate {
         }
         //set corners as equals
         cardCollection[12].setBackgroundImage(cardCollection[3].backgroundImage(for: .normal) , for: .normal)
+        cardsSelected[12] = cardsSelected[3]
     }
     
     @IBAction func selectCard(_ sender: UIButton) {
-        let overlay = UIView(frame: CGRect(x: 0, y: 0, width: sender.frame.width, height: sender.frame.height))
-        overlay.backgroundColor = UIColor.black
-        overlay.sizeToFit()
-        overlay.alpha = CGFloat(floatLiteral: 0.5)
-        sender.addSubview(overlay)
-//        sender.bringSubview(toFront: overlay)
-    }
-    
-    @IBAction func unselectCard( sender: UIView) {
+        let num = cardsSelected[sender.tag]
+        let dict = cardNames[num] as! NSDictionary
+        var image : UIImage!
         
+        if sender.isSelected {
+            // set deselected
+            image = UIImage(named: dict.value(forKey: "image") as! String)
+            sender.isSelected = false
+        } else {
+            // set selected
+            image = UIImage(named: dict.value(forKey: "bw") as! String)
+            sender.isSelected = true
+        }
+        sender.setBackgroundImage(image, for: .normal)
     }
     
     @IBAction func goBack(_ sender: Any) {
         view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    // Des Select all of the cards
+    @IBAction func eraseSelected(_ sender: Any) {
+        for card in cardCollection {
+            if card.isSelected {
+                let dict = cardNames[cardsSelected[card.tag]] as! NSDictionary
+                let image = UIImage(named: dict.value(forKey: "image") as! String)
+                card.setBackgroundImage(image, for: .normal)
+                card.isSelected = false
+            }
+        }
     }
     
     /*
